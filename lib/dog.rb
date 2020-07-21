3db29050a6ba6e49f2b4df75bbc9b1160b59ff72
Warda Ayaz <wayaz107@gmail.com>
2020-07-21 04:59:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+
+def name=(name)
+  @name_of_dogs = name  
+end 
+
+def name 
+  @name_of_dogs
+end 
+
+def breed= (breed)
+  @breed_of_dogs = breed
+end 
+
+def breed
+  @breed_of_dogs
+end 
+
+end 
+
